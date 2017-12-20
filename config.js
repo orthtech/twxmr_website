50688379bcfbb510d5ed3220f8e54bc25987bac3
root <root@twxmr-web.c.twxmr-189406.internal>
2017-12-20 14:29:39 +0000

    Initial commit

---
+++ config.js
@@ -0,0 +1,17 @@
+var api = "http://twxmr.tw:8117";
+
+var coinUnits = 1000000000000;
+
+var poolHost = "twxmr.tw";
+
+var irc = "irc.freenode.net/#monero-pools";
+
+var email = "snoopy831002@gmail.com";
+
+var cryptonatorWidget = ["XMR-BTC", "XMR-USD", "XMR-EUR", "XMR-GBP"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://chainradar.com/xmr/block/";
+
+var transactionExplorer = "http://chainradar.com/xmr/transaction/";
